3444df710ef9bbf2f6e0f4a94da1fb9b704e339f
Henderson Vicente Rodrigues <92233052+Seuvicente@users.noreply.github.com>
2023-01-30 15:49:15 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Robotron-2000
-Projeto desenvolvido durante o curso de JavaScript: Manipulando o DOM, na plataforma ALURA.
+# robotron-2000
---
+++ main.js
@@ -0,0 +1,66 @@
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+
+
+
+
+controle.forEach((elemento)=> {
+    elemento.addEventListener("click", (event) =>{
+        manipulaDados(event.target.dataset.controle, event.target.parentNode)
+        atualizaEstatisticas(event.target.dataset.peca)
+    })}
+)
+
+
+function manipulaDados(evento, controle){
+    const peca = controle.querySelector("[data-contador]");
+    if(evento === "+"){
+        peca.value = parseInt(peca.value)+1
+    }else{
+        peca.value = parseInt(peca.value)-1
+    }
+}
+
+function atualizaEstatisticas(peca){
+
+    estatisticas.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+}
